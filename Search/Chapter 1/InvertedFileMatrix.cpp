d9df44a5c3dd36802a249cea483b4b5662e3ad01
spicyfriedchicken <214891781+spicyfriedchicken@users.noreply.github.com>
2025-07-25 21:04:38 -0400

    ii

--- Search/Chapter 1/InvertedFileMatrix.cpp
+++ Search/Chapter 1/InvertedFileMatrix.cpp
@@ -2,16 +2,38 @@
 // Created by Oscar Abreu on 7/25/25.
 //
 
-int main() {
-    InvertedIndex ii;
-    std::string folderPath = "docs/";
-    for (const auto& entry : std::filesystem::directory_iterator(folderPath)) {
-        if (entry.path().extension() == ".txt") {
-            std::string fileName = entry.path().filename().string();
-            std::string content = readFileContent(entry.path().string());
-            std::vector<std::string> tokens = tokenize(content);
-            ii.addDocument(fileName, tokens);
+std::string readFileContents(std::string filePath) {
+    std::ifstream file(filePath);
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer;
+}
+
+std::vector<std::string> tokenize(const std::string contents) {
+    std::vector<std::string> tokens;
+    std::string temporary;
+
+    for (const auto& c : contents) {
+        if (isalnum(c)) temporary += (to_lower(c));
+        else if (!temporary.empty()) {
+            tokens.push_back(temporary);
+            temporary.clear();
         }
     }
-    ii.printMatrixToFile("ii.txt");
+    if (!temporary.empty()) tokens.push_back(temporary);
+    return tokens;
+}
+
+int main() {
+     InvertedIndex ii;
+      std::string folderPath = "./docs";
+      for (const auto& file :: std::filesystem::directory_iterator(folderPath)) {
+            if (file.extension().string() == ".txt") {
+                std::string fileName = file.path().filename().string();
+                std::string contents = readFileContents(file.content().string());
+                std::vector<std::string> tokens = tokenize(contents);
+                ii.AddDocument(fileName, tokens);
+               }
+      }
+      ii.PrintMatrixToCMDL("ii.txt");
 }
